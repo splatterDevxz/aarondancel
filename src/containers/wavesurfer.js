b39b3c36736e2eabe80434c5b2cf17dd11aaaa5b
___Justin::static <___justinstatic@MacBooks-MacBook-Pro.local>
2020-11-18 20:41:28 +0800

    11-18-20

--- src/containers/wavesurfer.js
+++ src/containers/wavesurfer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Helmet} from "react-helmet";
 
 class Audio extends React.Component {
 	constructor( props ){
@@ -9,12 +10,13 @@ class Audio extends React.Component {
 			reload:false
 		}
 	}
+
 			
   	render(){
   		return(	
   			<div className='container'>
   				<div className="wrapper-podcast-grid"> 
-  					<div id="buzzsprout-player-6414094"></div>
+  					<script src="https://www.buzzsprout.com/1493704/6414094-the-virtualpreneur-with-ruben-licera.js?container_id=buzzsprout-player-6414094&player=small" type="text/javascript" charset="utf-8"></script>
 			  	</div>
 		    </div>
   		)
